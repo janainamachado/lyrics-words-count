ae092fdb0d9f3ed6a90d2987133d5a654a97257b
Janaina Machado <janainamachado@Janainas-MBP.lan>
2024-11-01 14:27:46 -0300

    implement function getArtistSongs

--- server.js
+++ server.js
@@ -1,6 +1,39 @@
+require("dotenv").config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3000;
 
 const GENIUS_API_TOKEN = process.env.GENIUS_API_TOKEN;
+
+const getArtistSongs = async (artistId) =>
+{
+    try {
+        let songs = [];
+        let page = 1;
+        let hasMorePages = true;
+
+        while (hasMorePages) {
+            const response = await axios.get(`https://api.genius.com/artists/${artistId}/songs`,{
+                headers: {
+                    Authorization: `Bearer ${GENIUS_API_TOKEN}`,
+                },
+                params: {
+                        page: page,
+                } 
+            });
+            
+            const songsData = response.data.response.songs;
+            songs.push(...songsData);
+
+            if (songsData === 0){
+                hasMorePages = false;
+            }
+            page++;
+        }
+        return songs;
+    } catch (error){
+        console.error('Error fetching songs:', error.message);
+        return [];
+    }
+};
